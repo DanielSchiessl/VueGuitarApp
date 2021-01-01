baf23b2dd6efc49464734056298e320c089e84b1
DanielSchiessl <59205635+DanielSchiessl@users.noreply.github.com>
2021-01-01 23:02:06 +0100

    update noteselector and add get method

---
+++ App.vue
@@ -0,0 +1,51 @@
+<template>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld Welcome to Your Vue.js App"/> -->
+    <h1> VueJs Guitar App </h1>
+     <!--  <noteselector></noteselector> -->
+    <noteselectorv2></noteselectorv2>
+    <scalemodeselector></scalemodeselector>
+    <fretboard></fretboard>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import noteselector from './components/noteselector.vue'
+import noteselectorv2 from './components/noteselectorv2.vue'
+import scalemodeselector from './components/scalemodeselector.vue'
+import fretboard from './components/fretboard.vue'
+// import fretnote from './components/fretnote.vue'
+
+export default {
+    name: 'App',
+    // data () {
+    //    return {
+    //        note_selected: 'C'
+    //    }
+    // },
+    components: {
+        // HelloWorld,
+        // fretnote,
+        // noteselector,
+        noteselectorv2,
+        scalemodeselector,
+        fretboard
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+body {
+    margin: 40px;
+}
+
+</style>
---
+++ components/scalemodeselector.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="scalemodeselection">
+        <p>Select the scale mode that is to be used:</p>
+        <div class="wrapper">
+            <button v-for="(mode, modeindex) in modenames" class="modebutton" :key="modeindex" :style="{'grid-row': 1,'grid-column': modeindex+1}" :id="mode" v-on:click="select($event)">
+                {{mode}}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'scalemodeselector',
+    methods: {
+        select: function (event) {
+            var targetId = event.currentTarget.id
+            console.log(targetId) // the ID is equal to the notename to be used
+        }
+    },
+    data () {
+        return {
+            modenames: ['ionian (major)', 'dorian', 'phrygian', 'lydian', 'mixlydian', 'aeolian (minor)', 'locrian']
+        }
+    }
+}
+</script>
+
+<style scoped>
+.wrapper {
+    display: inline-grid;
+    grid-gap: 10px;
+    background-color: #fff;
+    color: #444;
+    z-index: -1;  /* to be below the parent element */
+    }
+
+.modebutton {
+    width: 150px;
+    font-size : 20px;
+    font-family: Calibri;
+}
+</style>
