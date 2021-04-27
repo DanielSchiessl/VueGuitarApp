12c5f6309eb58f103cb7d34a540519e037804b9a
Daniel Schiessl <59205635+DanielSchiessl@users.noreply.github.com>
2021-04-27 16:07:33 +0200

    Remaining local windows commits

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import axios from 'axios'
+
+// import router from './router'
+// import store from './store'
 
 createApp(App).mount('#app')
+// createApp(App).use(store).use(router).mount('#app')
+
+// axios.defaults.baseURL = 'http://localhost:8000/api/'
+
+// when testing with the flask serve we need to add the // as vue/axios otherwise requests http://localhost:8081/127.0.0.1:5000/xxx
+axios.defaults.baseURL = '//127.0.0.1:5000'
+// axios.defaults.withCredentials = true
